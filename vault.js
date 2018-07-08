7fa49f9d2cc5a958681808e2fcca7b65cdfdc39a
wyminc <wyminc808@gmail.com>
2018-07-07 17:13:38 -1000

    Did it

--- vault.js
+++ vault.js
@@ -1,4 +1,26 @@
 'use strict';
 module.exports = function() {
-  
+
+    const vault = {};
+
+    const setValue = function(key, value) {
+        if ((!key)){
+            return null;
+        } else {
+            vault[key] = value;
+        }
+    }
+
+    const getValue = function(key) {
+        if((!key) || (typeof vault[key] == "undefined")) {
+            return null;
+        } else {
+            return vault[key];
+        }
+    }
+
+    return {
+        setValue: setValue,
+        getValue: getValue
+    }
 };
